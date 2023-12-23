45b9cc8fc0308beaa2377450ad806ab6694c0f8c
PATHucd <140978146+PATHucd@users.noreply.github.com>
2023-12-22 16:25:34 -0800

    Update about.R

--- R/about.R
+++ R/about.R
@@ -10,8 +10,7 @@ aboutUI <- function(id) {
       p("This Shiny app was created so users could explore, filter, and download PATH data."),
       p("The Stations tab allows users to filter data by species, year, project and tag and visualize receiver locations. "),
       p("The Summary tab lets users filter and download metadata with summary detection data."),
-      p("The Tag History tab allows users to track selected tagged fish movement between receivers."),
-      p(" The Raw Detections tab directs users to links for downloading raw detection files that are to large to download on this app.")   
+      p("The Tag History tab allows users to track selected tagged fish movement between receivers.")  
          
        
     )
