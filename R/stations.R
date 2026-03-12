fa36a6766bccad0cd697a0e36cd557abc0e9a50e
jonathan-walter <jawalter@ucdavis.edu>
2026-03-12 14:44:40 -0700

    general bug fixes

--- R/stations.R
+++ R/stations.R
@@ -29,7 +29,7 @@ stationsServer <- function(id, data_query, parent) {
 
       output$map <- renderLeaflet({
         
-        req(input$species, input$year, input$locations, input$project, input$tag_code)
+        #req(input$species, input$year, input$locations, input$project, input$tag_code)
 
         leaflet() |>
           addProviderTiles("Esri.WorldGrayCanvas", group = "Gray") |>
